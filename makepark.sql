9d3b4da7e6076ae5bcd803bad64c23f64e165104
nghi01 <nnguyen@oberlin.edu>
2023-02-18 23:02:54 -0500

    make park sql

---
+++ makepark.sql
@@ -0,0 +1,20 @@
+CREATE TABLE park (
+	parkID int,
+	parkname varchar(255),
+    areaname varchar(255),
+    areaID varchar(255),
+    date_happened date,
+    state_time Time,
+    end_time Time,
+    total_time int,
+    park_cond varchar(255),
+    other_anim varchar(255),
+    litter varchar(255),
+    temp varchar(255),
+    num_squirrels int,
+    sighters varchar(255),
+    num_sighters int);
+
+    
+    
+    
